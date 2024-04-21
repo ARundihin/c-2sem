08e89db1f926ffd907b4e8403b206872f7ee9393
ARundihin <142354563+ARundihin@users.noreply.github.com>
2024-04-22 00:38:26 +0300

    Add files via upload

---
+++ List.cpp
@@ -0,0 +1,11 @@
+#include "List.h"
+
+template<class T>
+void List<T> :: SetFirstElement(T value)
+{
+	/*Node* newnode = new Node;
+	newnode->data = data;
+	newnode->next = nullptr;
+    this->head = newnode;
+    this->size++;*/
+}
